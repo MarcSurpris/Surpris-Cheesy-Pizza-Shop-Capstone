a00104545133c3c4c4980abb38d010afb3f947f0
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-12 11:23:59 -0500

    This is the constructor for the Side class.

--- src/main/java/com/pluralsight/capstone/model/Side.java
+++ src/main/java/com/pluralsight/capstone/model/Side.java
@@ -1,4 +1,10 @@
 package com.pluralsight.capstone.model;
+
+import com.pluralsight.capstone.util.ToppingCategory;
+
 // Side
 public class Side extends Topping {
+    public Side(String name) {
+        super(name, ToppingCategory.SIDE);
+    }
 }
