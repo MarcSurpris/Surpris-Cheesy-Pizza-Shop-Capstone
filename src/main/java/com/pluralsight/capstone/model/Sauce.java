19d6f513804a28ac444d2e5656358170bada8530
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-12 10:53:36 -0500

    This is the constructor for the Sauce class.

--- src/main/java/com/pluralsight/capstone/model/Sauce.java
+++ src/main/java/com/pluralsight/capstone/model/Sauce.java
@@ -1,4 +1,10 @@
 package com.pluralsight.capstone.model;
+
+import com.pluralsight.capstone.util.ToppingCategory;
+
 // Sauce
 public class Sauce extends Topping {
+    public Sauce(String name) {
+        super(name, ToppingCategory.SAUCE);
+    }
 }
