8e126ebb4ca465807ef9c31b898b518e1b3d318e
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-13 11:46:01 -0500

    These lines define public static final String constants for standard size for standard size options for the drinks.

--- src/main/java/com/pluralsight/capstone/util/DrinkSize.java
+++ src/main/java/com/pluralsight/capstone/util/DrinkSize.java
@@ -1,4 +1,11 @@
 package com.pluralsight.capstone.util;
 
+// DrinkSize class with constants
+
 public class DrinkSize {
+    public static final String SMALL = "SMALL";
+    public static final String MEDIUM = "MEDIUM";
+    public static final String LARGE = "LARGE";
+
+
 }
