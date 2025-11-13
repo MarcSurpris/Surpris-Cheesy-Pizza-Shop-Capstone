7da552ea8e474b494b32d0cbf804ebf9a42e317b
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-13 15:44:12 -0500

    These lines define private fields and a constructor in a class.

--- src/main/java/com/pluralsight/capstone/model/GarlicKnot.java
+++ src/main/java/com/pluralsight/capstone/model/GarlicKnot.java
@@ -1,4 +1,10 @@
 package com.pluralsight.capstone.model;
 // GarlicKnot
 public class GarlicKnot implements Item {
+    private int quantity;
+    private static final double PRICE_PER = 1.50;
+
+    public GarlicKnot(int quantity) {
+        this.quantity = quantity;
+    }
 }
